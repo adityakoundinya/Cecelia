01ae2674c1fb52ecf1dee32e160b4de090ae18e6
Aditya Koundinya <aditya.koundinya@gmail.com>
2012-02-29 19:35:36 +0000

    Updated Version Number.

--- CeceliaWebsite/CeceliaMarketPlaceDatabase/CeceliaMarketPlaceDatabase/AssemblyInfo/Release/AssemblyInfo.cs
+++ CeceliaWebsite/CeceliaMarketPlaceDatabase/CeceliaMarketPlaceDatabase/AssemblyInfo/Release/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-[assembly: AssemblyFileVersion("1.2.0.1")]
-[assembly: AssemblyVersion("1.2.0.1")]
+[assembly: AssemblyFileVersion("1.2.0.2")]
+[assembly: AssemblyVersion("1.2.0.2")]
